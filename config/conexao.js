4ae77078a6b5da6fdb43fc4593a9602c9e140391
Alberto <lff9567@gmail>
2024-09-26 14:41:12 -0300

    dia 26

--- config/conexao.js
+++ config/conexao.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 
 const conexao = await mongoose.connect("mongodb://aluno:aluno@alberto-shard-00-00.zfrsc.mongodb.net:27017,alberto-shard-00-01.zfrsc.mongodb.net:27017,alberto-shard-00-02.zfrsc.mongodb.net:27017/?ssl=true&replicaSet=atlas-olm0ol-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Alberto")
+//const conexao = await mongoose.connect('mongodb://localhost:27017/ifstore')
 
 export default conexao
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,11 @@
- import Usuario from '../models/usuario.js';
+import Usuario from '../models/usuario.js';
 
-export async function listarusuarios (req, res){
+export async function listarusuarios(req, res){
     const usuarios = await Usuario.find({});
     res.render('admin/usuarios/lst', {usuarios: usuarios});
-
 }
+
+export async function detalhe(req, res){
+     const usuario = await Usuario.findById(req.params.id);
+     res.render('admin/usuarios/detalhe', {usuario: usuario});
+}    
