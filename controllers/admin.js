69bf1cca912f8d6f20e86bc068a5cde9634034ec
Alberto <lff9567@gmail>
2024-10-03 14:51:25 -0300

    aula 3 out

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,38 @@
 import Usuario from '../models/usuario.js';
+import Categoria from '../models/categoria.js';
 
 export async function listarusuarios(req, res){
-    const usuarios = await Usuario.find({});
+    console.log("AQUI"+req.params)
+    const usuarios = await Usuario.find({}).catch(function(err){console.log(err)});
     res.render('admin/usuarios/lst', {usuarios: usuarios});
 }
 
-export async function detalhe(req, res){
-     const usuario = await Usuario.findById(req.params.id);
-     res.render('admin/usuarios/detalhe', {usuario: usuario});
-}    
+export async function detalhe(req, res) {
+    const usuario = await Usuario.findById(req.params.id);
+    res.render('admin/usuarios/detalhe', {usuario: usuario});
+}
+
+export async function abreaddcategoria(req, res) {
+    res.render('admin/categoria/add')
+}
+
+export async function addcategoria(req, res) {
+    await Categoria.create({
+        nome:req.body.nome
+    })
+    res.redirect('/admin/categoria/add')
+}
+
+export async function listarcategoria(req, res) {
+    const categorias = await Categoria.find({})
+    res.render('admin/categoria/lst',{Categorias: categorias});
+}
+
+export async function filtrarcategoria(req, res) {
+    const categorias = await Categoria.find({nome: new RegExp (req.body.pesquisar,"1")})
+    res.render('admin/categoria/lst',{Categorias: categorias});
+}
+export async function deletacategoria(req, res) {
+    await Categoria.findByIdAndDelete(req.params.id)
+    res.redirect('/admin/categoria/lst')
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,21 @@
 import express from 'express'
 const router = express.Router()
 
-import {listarusuarios,detalhe} from '../controllers/admin.js';
+import {listarusuarios, detalhe, abreaddcategoria, addcategoria, listarcategoria,filtrarcategoria,deletacategoria} from '../controllers/admin.js';
 
 router.get("/admin/usuarios/lst", listarusuarios)
 router.get("/admin/usuarios/detalhe/:id", detalhe)
+
+//create do modelo categoria (create)
+router.get('/admin/categoria/add', abreaddcategoria)
+router.post('/admin/categoria/add', addcategoria)
+
+//rotas do modelo categoria(read)
+router.get('/admin/categoria/lst', listarcategoria)
+router.post('/admin/categoria/lst', filtrarcategoria)
+
+//rotas do modelo categoria(delete)
+router.get('/admin/categoria/del/:id', deletacategoria)
+
 
 export default router
